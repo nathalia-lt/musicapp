aedfad786afc1fbe4f097914ce290f92b85374b3
nathalia <nathaliatroina@gmail.com>
2023-11-07 18:51:16 -0500

    home page

---
+++ _exemplos/exemplos.js
@@ -0,0 +1,58 @@
+const Test = (props, $el, $update, $f7, $on = 5, $x = 10 ) => {
+    
+
+    return $render;
+}
+
+const props = {id: 58, color: black}
+const $el = function () {
+    return 
+}
+
+const $update = function () {
+    return 
+}
+
+const $f7 = {
+    on: function () {
+        return 
+    },
+    dialog:  { },
+}
+
+
+
+
+Test(props, $el, , $f7, null, null, ...., null, $x )
+
+
+
+
+const NovaFunc = (a, b, c) => {
+    return a + b + c;
+}
+
+pessoa = {b: 3, a: 2}
+
+notas = {c: 5, b: 4, a: 3}
+
+end = {a: 1, b: 2, c: 3}
+//se o key e o value sao os mesmos nao precisa repetir
+info = {pessoa, notas, end }
+
+const NovaFunc2 = ( {pessoa, notas, end } ) => {
+    
+}
+//eu tenho a opcao de usar so o que eu quero daquele objeto, no caso end.
+const NovaFunc3 = ( { end } ) => {
+    
+}
+
+const NovaFunc4 = ( { notas } ) => {
+    
+}
+
+
+NovaFunc2(info)
+NovaFunc3(info)
+NovaFunc4(info)
