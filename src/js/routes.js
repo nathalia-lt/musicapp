8bc13455f9ab3270f1c128acb52acacc7470b389
nathalia <nathaliatroina@gmail.com>
2023-11-14 18:13:34 -0500

    toolbar

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,12 +1,9 @@
 
 import HomePage from '../pages/home.f7';
-import AboutPage from '../pages/about.f7';
+import SearchPage from '../pages/search.f7';
 import FormPage from '../pages/form.f7';
 import FavoritesPage from '../pages/favorites.f7'
 
-
-import DynamicRoutePage from '../pages/dynamic-route.f7';
-import RequestAndLoad from '../pages/request-and-load.f7';
 import NotFoundPage from '../pages/404.f7';
 
 var routes = [
@@ -15,8 +12,8 @@ var routes = [
     component: HomePage,
   },
   {
-    path: '/about/',
-    component: AboutPage,
+    path: '/search/',
+    component: SearchPage,
   },
   {
     path: '/form/',
@@ -24,59 +21,8 @@ var routes = [
   },
 
   {
-    path: '/dynamic-route/blog/:blogId/post/:postId/',
-    component: DynamicRoutePage,
-  },
-  {
     path: '/favorites/',
     component: FavoritesPage,
-  },
-  {
-    path: '/request-and-load/user/:userId/',
-    async: function ({ router, to, resolve }) {
-      // App instance
-      var app = router.app;
-
-      // Show Preloader
-      app.preloader.show();
-
-      // User ID from request
-      var userId = to.params.userId;
-
-      // Simulate Ajax Request
-      setTimeout(function () {
-        // We got user data from request
-        var user = {
-          firstName: 'Vladimir',
-          lastName: 'Kharlampidi',
-          about: 'Hello, i am creator of Framework7! Hope you like it!',
-          links: [
-            {
-              title: 'Framework7 Website',
-              url: 'http://framework7.io',
-            },
-            {
-              title: 'Framework7 Forum',
-              url: 'http://forum.framework7.io',
-            },
-          ]
-        };
-        // Hide Preloader
-        app.preloader.hide();
-
-        // Resolve route to load page
-        resolve(
-          {
-            component: RequestAndLoad,
-          },
-          {
-            props: {
-              user: user,
-            }
-          }
-        );
-      }, 1000);
-    },
   },
   {
     path: '(.*)',
